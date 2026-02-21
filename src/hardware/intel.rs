3ee4a82bd37513363147812a65a84d3bdf16c742
sakuyma <yerkin.timofey@protonmail.com>
2026-02-21 21:23:49 +0300

    CHORE: add intel drivers setup variant

---
+++ src/hardware/intel.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+
+fn install_drivers() {
+    let cmd = format!("sudo pacman -S --noconfirm --needed");
+    let status = Command::new("pacman")
+        .args(&[
+            "-S",
+            "--noconfirm",
+            "--needed",
+            "mesa",
+            "lib32-mesa",
+            "xf86-video-intel",
+            "lib32-vulcan-intel",
+        ])
+        .status()?;
+    Ok(status.success())
+}
+
+pub fn install() {
+    install_drivers();
+}
